b5a421c340f0e334b9064cf8176283e3601df4d9
Pete Tian <petian@microsoft.com>
2019-08-19 18:31:07 -0500

    Upgrade to spring boot 2.1.7

--- src/main/java/com/jhaexample/demo/Dto/InputDto.java
+++ src/main/java/com/jhaexample/demo/Dto/InputDto.java
@@ -1,28 +1,17 @@
 package com.jhaexample.demo.Dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class InputDto {
 	int i;
 	String s;
-
-	public int getI() {
-		return i;
-	}
-
-	public void setI(int i) {
-		this.i = i;
-	}
-
-	public String getS() {
-		return s;
-	}
-
-	public void setS(String s) {
-		this.s = s;
-	}
 
 	@Override
 	public String toString() {
 		return "InputDto [i=" + i + ", s=" + s + "]";
 	}
-	
+
 }
