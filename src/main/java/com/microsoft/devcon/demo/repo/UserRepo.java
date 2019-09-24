767b3b8737fe57f38d85fd496859da313cddc40f
Pete Tian <petian@microsoft.com>
2019-09-24 14:32:49 -0500

    switch to CrudRepository

--- src/main/java/com/microsoft/devcon/demo/repo/UserRepo.java
+++ src/main/java/com/microsoft/devcon/demo/repo/UserRepo.java
@@ -2,10 +2,10 @@ package com.microsoft.devcon.demo.repo;
 
 import java.util.List;
 
-import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.CrudRepository;
 
 import com.microsoft.devcon.demo.entity.User;
 
-public interface UserRepo extends PagingAndSortingRepository<User, Long>{
+public interface UserRepo extends CrudRepository<User, Long> {
 	public List<User> findAll();
 }
