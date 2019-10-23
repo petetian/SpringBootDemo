5f9f7a74d9945b859c4b228603568e92b07e7bc0
Pete Tian <petian@microsoft.com>
2019-10-23 11:34:50 -0500

    Spring Boot starter: upgrade to 2.2.0

--- src/main/java/com/microsoft/devcon/demo/DemoApplication.java
+++ src/main/java/com/microsoft/devcon/demo/DemoApplication.java
@@ -1,13 +1,24 @@
 package com.microsoft.devcon.demo;
 
+import javax.annotation.PreDestroy;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
+	private static final Logger logger = LoggerFactory.getLogger(DemoApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+
+	}
+
+	@PreDestroy
+	public void tearDown() {
+		logger.info(">>>>>>>>>>>>>>>> Shutting Down the app <<<<<<<<<<<<<<<<<");
 	}
 
 }
