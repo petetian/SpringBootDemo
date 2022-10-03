cef218dbbd6b1d8ce929000a44aaec4810342e05
Pete T <pete.tian@gmail.com>
2022-10-03 12:36:02 -0700

    Remove swegger2

--- src/main/java/com/microsoft/devcon/demo/SecurityConfig.java
+++ src/main/java/com/microsoft/devcon/demo/SecurityConfig.java
@@ -4,10 +4,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
 @Configuration
-@EnableSwagger2
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	  protected void configure(HttpSecurity http) throws Exception {
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,13 +1,11 @@
-DROP TABLE IF EXISTS user;
  
-CREATE TABLE user (
-  id INT AUTO_INCREMENT  PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS `user` (
   first_name VARCHAR(250) NOT NULL,
   last_name VARCHAR(250) NOT NULL,
   account_number VARCHAR(250) DEFAULT NULL
 );
 
 
-INSERT INTO user (first_name, last_name, account_number) VALUES
+INSERT INTO `user` (first_name, last_name, account_number) VALUES
   ('John', 'Doe', 'A1238374'),
   ('Jane', 'Smith', 'A09458584');
