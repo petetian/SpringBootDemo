86a906b04f99458abe944f80c77f4d3a3da95a75
petian <petian@10.0.0.209>
2022-11-08 10:03:20 -0800

    Enable service discovery: eurika

--- src/main/java/com/microsoft/devcon/demo/DemoApplication.java
+++ src/main/java/com/microsoft/devcon/demo/DemoApplication.java
@@ -6,8 +6,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class DemoApplication {
 	private static final Logger logger = LoggerFactory.getLogger(DemoApplication.class);
 
