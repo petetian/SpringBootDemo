d7c3cef643709acb58dd2800c0525cacc03adf22
petian <petian@10.0.0.209>
2022-11-11 15:15:03 -0800

    add separate profile for azure app service to skip service registration

--- src/main/java/com/microsoft/devcon/demo/DemoApplication.java
+++ src/main/java/com/microsoft/devcon/demo/DemoApplication.java
@@ -6,10 +6,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableEurekaClient
 public class DemoApplication {
 	private static final Logger logger = LoggerFactory.getLogger(DemoApplication.class);
 
@@ -22,5 +20,6 @@ public class DemoApplication {
 	public void tearDown() {
 		logger.info(">>>>>>>>>>>>>>>> Shutting Down the app <<<<<<<<<<<<<<<<<");
 	}
-
+	
 }
+
