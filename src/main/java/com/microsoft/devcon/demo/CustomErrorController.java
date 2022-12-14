8d5b41fafbcc2c494dc2bff7b5a6accde1649dc7
petian <petian@host.docker.internal>
2022-12-13 18:10:28 -0800

    Add error fall back page

---
+++ src/main/java/com/microsoft/devcon/demo/CustomErrorController.java
@@ -0,0 +1,23 @@
+package com.microsoft.devcon.demo;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CustomErrorController implements ErrorController {
+
+
+  @RequestMapping("/error")
+  @ResponseBody
+  String error(HttpServletRequest request) {
+    return "<h1>Error occurred</h1>";
+  }
+
+  public String getErrorPath() {
+    return "/error";
+  }
+}
